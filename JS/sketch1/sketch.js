99f56485c8b93d6da322f05e68b5e461db9db973
Manan Dua <manandua01@gmail.com>
2021-04-11 14:40:22 +0530

    made 1st sketch

--- JS/sketch1/sketch.js
+++ JS/sketch1/sketch.js
@@ -1,10 +1,47 @@
-function setup(){
-  // put setup code here
-createCanvas(400, 400);
+function setup() {
+  createCanvas(400, 400);
+  strokeWeight(0);
 }
 
-function draw(){
-  // put drawing code here
-background(200);
-ellipse(200, 200, 50);
+function draw() {
+	background('#ffedc9');
+  
+  
+  fill('#00c95a');
+  
+  // body
+  rect(145, 174, 110, 92);
+
+      fill('#00b04e');
+  
+  // hands
+  rect(123, 205, 39, 19);
+  rect(233, 205, 39, 19);
+
+
+    fill('#00b04e');
+  
+  // head
+  circle(200, 156, 100)
+  
+  // left eye
+  fill(255);
+  circle(165, 134, 25);
+  strokeWeight(1);
+  fill(0);
+  circle(165, 134, 10);
+  
+  // right eye
+  fill(255);
+  circle(234, 134, 25);
+  fill(0);
+  circle(234, 134, 10);
+  
+  
+    fill('#00b04e');
+  
+  // legs
+  rect(111, 260, 79, 29);
+  rect(211, 260, 79, 29);
+
 }
