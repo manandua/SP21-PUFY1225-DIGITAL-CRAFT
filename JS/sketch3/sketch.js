65fa346eb4d26a27df3fc0569a2472951c1bdfcb
Manan Dua <manandua01@gmail.com>
2021-04-12 01:48:44 +0530

    sketch 3

---
+++ JS/sketch3/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(400, 400);
+  strokeWeight(0);
+}
+
+function draw() {
+	background('#d78a10');
+  
+  
+  fill('#ff9c00');
+
+  triangle(30, 90, 58, 220, 106, 100);
+
+  ellipse(200, 200, 320, 250); 
+
+
+  fill('#f3f3f3');
+  circle(166, 120, 50);
+  fill('#454539');
+  circle(166, 120, 25);
+
+
+}
