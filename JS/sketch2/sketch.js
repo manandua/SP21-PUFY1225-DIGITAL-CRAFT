162becf7a001b5fa230fe742182b12fcef88e3d8
Manan Dua <manandua01@gmail.com>
2021-04-12 01:15:27 +0530

    added sketch2

---
+++ JS/sketch2/sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+  createCanvas(400, 400);
+  strokeWeight(0);
+}
+
+function draw() {
+	background('#2b5ba5');
+  
+  
+  fill('#454539');
+  
+  // body
+  ellipse(200, 240, 280);
+  ellipse(200, 120, 150);
+  ellipse(168,50,40,75);
+  ellipse(228,50,40,75);
+
+
+  fill('#c8c18c');
+
+  ellipse(200, 270, 220);
+
+  fill('#f3f3f3');
+  circle(166, 80, 20);
+  fill('#454539');
+  circle(166, 80, 10);
+
+  fill('#f3f3f3');
+  circle(230, 80, 20);
+  fill('#454539');
+  circle(230, 80, 10);
+  fill('black');
+  ellipse(200,90,25,8);
+  fill('#454539');
+  ellipse(150,348,60,85);
+  ellipse(250,348,60,85);
+  fill('#c8c18c');
+  ellipse(150,360,20,25);
+  ellipse(250,360,20,25);
+}
